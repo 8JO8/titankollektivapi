230386f382eb08fb1694d51f4f51e57e416a2968
8JO8 <76175269+8JO8@users.noreply.github.com>
2024-11-21 18:56:11 +0100

    Upgraded API to v2 - main.py

--- main.py
+++ main.py
@@ -23,110 +23,65 @@ class Entry(db.Model):
     Month = db.Column(db.String(3))
     Time = db.Column(db.String(50))
     ImageLink = db.Column(db.String(150))
+    TicketLink = db.Column(db.String(150), default='https://titankollektiv.de/tickets')
     State = db.Column(db.Integer, default=0)
 
 # Create tables if not exists
 with app.app_context():
     db.create_all()
 
-# Authentication decorator
-def createtoken_required(f):
-    def createdecorated(*args, **kwargs):
-        token = request.args.get('token')
-
-        if not token:
-            return jsonify({'message': 'Token is missing!'}), 401
-
-        # You can implement your own token verification logic here
-        if token != os.getenv("CreateToken"):
-            return jsonify({'message': 'Invalid token!'}), 401
-
-        return f(*args, **kwargs)
-
-    return createdecorated
-
-
-def deletetoken_required(f):
-    def deletedecorated(*args, **kwargs):
-        token = request.args.get('token')
-
-        if not token:
-            return jsonify({'message': 'Token is missing!'}), 401
-
-        # You can implement your own token verification logic here
-        if token != os.getenv("DeleteToken"):
-            return jsonify({'message': 'Invalid token!'}), 401
-
-        return f(*args, **kwargs)
-
-    return deletedecorated
-
-def gettoken_required(f):
-    def getdecorated(*args, **kwargs):
-        token = request.args.get('token')
-
-        if not token:
-            return jsonify({'message': 'Token is missing!'}), 401
-
-        # You can implement your own token verification logic here
-        if token != os.getenv("GetToken"):
-            return jsonify({'message': 'Invalid token!'}), 401
-
-        return f(*args, **kwargs)
-
-    return getdecorated
-
-# Endpoint to create a new entry
-@app.route('/api/event/create', methods=['POST'])
-@createtoken_required
-def create_entry():
-    data = request.get_json()
-
-    new_entry = Entry(
-        Name=data['name'],
-        Description=data['description'],
-        Location=data['location'],
-        Day=data['day'],
-        Month=data['month'],
-        Time=data['time'],
-        ImageLink=data['image_link']
-    )
-
-    with app.app_context():
-        try:
-            db.session.add(new_entry)
-            db.session.commit()
-            return jsonify({'message': 'Entry created successfully!'}), 201
-        except IntegrityError:
-            db.session.rollback()
-            return jsonify({'message': 'An error occurred while creating the entry. Please check your data.'}), 400
-
-
-# Endpoint to delete an entry
-@app.route('/api/event/delete/<int:entry_id>', methods=['DELETE'])
-@deletetoken_required
-def delete_entry(entry_id):
-    entry = Entry.query.get(entry_id)
-    if not entry:
-        return jsonify({'message': 'Entry not found!'}), 404
-
-    entry.State = 255
-    try:
-        db.session.commit()
-        return jsonify({'message': 'Entry deleted successfully!'}), 200
-    except Exception as e:
-        db.session.rollback()
-        return jsonify({'message': 'An error occurred while deleting the entry.'}), 500
-
-# Endpoint to retrieve all entry IDs with status equal to 0, along with their Name and Location
-@app.route('/api/event/getall', methods=['GET'])
-@gettoken_required
+# Endpoint to retrieve all entryss with status equal to 0
+@app.route('/api/v2/event/get/active', methods=['GET'])
 def get_entries():
     try:
         entries = Entry.query.filter_by(State=0).all()
         entry_list = [
             {
                 'id': entry.Id,
+                'name': entry.Name,
+                'location': entry.Location,
+                'day': entry.Day,
+                'monthString': entry.Month,
+                'friendlyText': f"Nummer: {entry.Id} | {entry.Name} in {entry.Location} am {entry.Day}.{entry.Month}"
+            }
+            for entry in entries
+        ]
+        return jsonify({'entries': entry_list}), 200
+    except Exception as e:
+        return jsonify({'message': 'An error occurred while retrieving entries.'}), 500
+
+# Endpoint to retrieve all entrys with status equal to 255
+@app.route('/api/v2/event/get/inactive', methods=['GET'])
+def get_entries():
+    try:
+        entries = Entry.query.filter_by(State=255).all()
+        entry_list = [
+            {
+                'id': entry.Id,
+                'name': entry.Name,
+                'location': entry.Location,
+                'day': entry.Day,
+                'monthString': entry.Month,
+                'friendlyText': f"Nummer: {entry.Id} | {entry.Name} in {entry.Location} am {entry.Day}.{entry.Month}"
+            }
+            for entry in entries
+        ]
+        return jsonify({'entries': entry_list}), 200
+    except Exception as e:
+        return jsonify({'message': 'An error occurred while retrieving entries.'}), 500
+
+# Endpoint to retrieve all entrys
+@app.route('/api/v2/event/get/all', methods=['GET'])
+def get_entries():
+    try:
+        entries = Entry.query.all()
+        entry_list = [
+            {
+                'id': entry.Id,
+                'name': entry.Name,
+                'location': entry.Location,
+                'day': entry.Day,
+                'monthString': entry.Month,
                 'friendlyText': f"Nummer: {entry.Id} | {entry.Name} in {entry.Location} am {entry.Day}.{entry.Month}"
             }
             for entry in entries
